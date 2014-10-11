24ac908d660e89f0cdbabc08addb19895114566c
presher <pre12014@byui.edu>
2014-10-11 17:39:18 -0500

    https://www.youtube.com/watch?v=njEctoLifiE&list=UUEGrHEke9e7ab-XG35HyKLA

--- src/galtolit/java/GalToLit.java
+++ src/galtolit/java/GalToLit.java
@@ -11,19 +11,16 @@ package galtolit.java;
  */
 //Program Start
 public class GalToLit {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         double gallons; //use double for more precise #'s 0.00
-        double liters;//use int for less precise #'s 0
-        
-        gallons = 10;
+        double liters;//use int for less precise #'s 0        
+        gallons = 100;
         //convert gallons to liters
-        liters = gallons * 3.7854;
-        
+        liters = gallons * 3.7854;       
         System.out.println(gallons + " gallons is: " + liters + " liters. ");
         //end of program
     }
